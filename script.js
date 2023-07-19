e6d015f239dfd61745612bc9ea5f9e78b2ba8b41
MuzanCoder <136102700+MuzanCoder@users.noreply.github.com>
2023-07-19 12:28:17 +0530

    This is Simple version gonna add more thing after doing some research on my own. hope you enjoy this :)

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const dateofBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your birthday is lucky🤗";
+    } else {
+        outputBox.innerText = "Your Birthdate is not lucky💀";
+    }
+}
 
 function checkBirthdateIsLucky() {
     const dob = dateofBirth.value;
     const sum = calculatedSum(dob);
-    console.log(sum);
+    const luckyNum = parseInt(luckyNumber.value);
+
+    if (!isNaN(sum) && !isNaN(luckyNum) && dob) {
+        compareValues(sum, luckyNum);
+    } else {
+        outputBox.innerText = "Please enter valid inputs";
+    }
 }
 
 function calculatedSum(dob) {
     dob = dob.replaceAll("-", "");
     let sum = 0;
-    for (let index = 0;index<dob.length; index++){
-        sum = sum + Number(dob.charAt(index));
-    }
-    return sum;
 
+    for (let index = 0; index < dob.length; index++) {
+        const digit = parseInt(dob.charAt(index));
+        if (!isNaN(digit)) {
+            sum += digit;
+        }
+    }
+
+    return sum;
 }
 
-
-
-checkNumberButton.addEventListener('click', checkBirthdateIsLucky)
+checkNumberButton.addEventListener('click', checkBirthdateIsLucky);
